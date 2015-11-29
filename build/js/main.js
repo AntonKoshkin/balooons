d8927d746e8ba1bcd1780923b51cd3f3a7328dcc
scink <a.v.koshkin.91@gmail.com>
2015-11-30 00:44:24 +0300

    0.44 30.11

--- build/js/main.js
+++ build/js/main.js
@@ -9208,3 +9208,12 @@ if ( typeof noGlobal === strundefined ) {
 return jQuery;
 
 }));
+
+jQuery(document).ready(function($) {
+	$('.side-menu__item').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		$('.side-menu__sublist').removeClass('side-menu__sublist--active');
+		$(this).find('.side-menu__sublist').addClass('side-menu__sublist--active');
+	});
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 //= ../../bower_components/jquery/dist/jquery.js
+
+jQuery(document).ready(function($) {
+	$('.side-menu__item').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		$('.side-menu__sublist').removeClass('side-menu__sublist--active');
+		$(this).find('.side-menu__sublist').addClass('side-menu__sublist--active');
+	});
+});
