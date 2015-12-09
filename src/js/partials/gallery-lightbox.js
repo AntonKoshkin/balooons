4e39f15e326d512040c3c086290781fe2ca78191
scink <a.v.koshkin.91@gmail.com>
2015-12-09 16:04:37 +0300

    comments added

--- src/js/partials/gallery-lightbox.js
+++ src/js/partials/gallery-lightbox.js
@@ -1,16 +1,31 @@
+// обработка клика по миниатюре
 $('.cat-gal__link').on('click', function(event) {
+
+	// отменяем действие по умолчанию
 	event.preventDefault();
-	/* Act on the event */
+
+	// положим в переменную адрес большой картинки
 	var pic = $(this).find('.cat-gal__img').attr('data-img');
 
+	// добавляем его в адрес для картинки в модалке
 	$('.gal-box__img').attr('src', pic);
+
+	// добавляем класс видимости модалке
 	$('.gal-box').addClass('gal-box--on');
+
+	// отключаем скролл у страницы
 	$('html,body').css('overflow','hidden');
 });
 
+// обработка клика по кнопке закрытия и по фону
 $('.gal-box__close, .gal-box__back').on('click', function(event) {
+
+	//  отменяем действие по умолчанию
 	event.preventDefault();
-	/* Act on the event */
+
+	// удаляем класс видимости у модалки
 	$('.gal-box').removeClass('gal-box--on');
+
+	// возвращаем скролл странице
 	$('html,body').css('overflow','auto');
 });
